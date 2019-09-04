c7e56db594578d700c76841148165d2c3cb20c3a
Tony Ou <tonyou1999@hotmail.com>
2019-09-04 14:54:06 -0400

    Added graphing of found values

--- CodingChallenge.py
+++ CodingChallenge.py
@@ -1,5 +1,8 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
 def openCSV(p_file):
-    """Opens the txt file with path p_file and separated by delimiter delim. Returns a 2D list."""
+    """Opens the csv file with path p_file. Returns a 2D list."""
 
     file = open(p_file, 'r', encoding='utf-8-sig')
     data = [[n for n in line[:-1].split(',')] for line in file]  # DJ: Opens file without including \n character
@@ -34,13 +37,22 @@ if __name__ == '__main__':
             rent.append(float(loans[i]))
         else:
             own.append(float(loans[i]))
+
     mortgageMean = sum(mortgage)/len(mortgage)
     rentMean = sum(rent)/len(rent)
     ownMean = sum(own)/len(own)
 
-    print(mortgageMean)
-    print(rentMean)
-    print(ownMean)
+    means = [mortgageMean, ownMean, rentMean]
+    labels = ["MORTGAGE", "OWN", "RENT"]
+
+    index = np.arange(len(labels))
+    plt.bar(index, means, width=0.6)
+    plt.xlabel('Home ownership', fontsize=12)
+    plt.ylabel('Average loan amount ($)', fontsize=12)
+    plt.xticks(index, labels, fontsize=12)
+    plt.title('Average loan amounts per home ownership')
+    plt.show()
+
 
 
 
